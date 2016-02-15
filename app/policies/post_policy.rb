fe3f8d5f4773b0301dc2a135be70b36f7546b22c
katiesteiner <katiewsteiner@gmail.com>
2016-02-15 18:09:12 -0500

    Checkpoint 39 assignment

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -2,4 +2,18 @@ class PostPolicy < ApplicationPolicy
   def index?
     true
   end
+  class Scope
+    def resolve
+      if user.admin? || user.moderator?
+        scope.all
+      elsif user.member?
+        self.post
+      else
+        scope.where(published: true)
+      end
+    end
+    def updated?
+      user.admin? or not post.published?
+    end
+  end
 end
