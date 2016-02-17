2dd62f57c1f19347abe27550c7bbc5e5bc1225ec
katiesteiner <katiewsteiner@gmail.com>
2016-02-16 22:42:57 -0500

    Checkpoint 44 Assignment

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-class AvatarUploader < CarrierWave::Uploader::Base
+class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
@@ -26,7 +26,11 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
-  process :resize_to_fill => [200, 300]
+  process resize_to_fill: [300, 300]
+
+  version :thumb do
+    process resize_to_fill: [50, 50]
+  end
   # def scale(width, height)
   #   # do something
   # end
@@ -35,23 +39,12 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # version :thumb do
   #   process :resize_to_fit => [50, 50]
   # end
-  version :tiny do
-    process resize_to_fill: [20, 20]
-  end
-
-  version :small do
-    process resize_to_fill: [30, 30]
-  end
-
-  version :profile do
-    process resize_to_fill: [45, 45]
-  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  def extension_white_list
-    %w(jpg jpeg gif png)
-  end
+  # def extension_white_list
+  #   %w(jpg jpeg gif png)
+  # end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
