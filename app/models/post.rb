86d0c7aa317b519ae89435fa2fbf91df134a3983
katiesteiner <katiewsteiner@gmail.com>
2016-02-19 21:17:38 -0500

    Checkpoint 50

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,12 +9,11 @@ class Post < ActiveRecord::Base
   
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
-  #validates :topic, presence: true
-  #validates :user, presence: true
+  validates :topic, presence: true
+  validates :user, presence: true
 
   mount_uploader :image, ImageUploader
 
-  after_create :create_vote
 
   def up_votes
     votes.where(value: 1).count
@@ -34,8 +33,6 @@ class Post < ActiveRecord::Base
 
     update_attribute(:rank, new_rank)
   end
-
-  private
 
   def create_vote
     user.votes.create(value: 1, post: self)
