9bb737610b3d10312839c0cf9514f7f183a5644a
katiesteiner <katiewsteiner@gmail.com>
2016-02-21 20:54:19 -0500

    Checkpoint 54 Assignment

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -1,16 +1,30 @@
 require 'rails_helper'
 
-describe "Visiting profiles" do 
+describe "Visiting profiles" do
   include TestFactories
-  before do
+  include Warden::Test::Helpers
+  Warden.test_mode!
+
+  before do 
     @user = authenticated_user
-    @post = associated_post(user: @user)
-    @comment = Comment.new(user: @user, post: @post, body: "A Comment")
-    allow(@comment).to receive(:send_favorite_emails)
-    @comment.save!
+    @topic = Topic.create(description: "A topic")
+    @post = post_without_user(user: @user, topic: @topic)
+    @comment = comment_without_email(post: @post, user: @user)
   end
 
   describe "not signed in" do
+    it "shows profile" do
+      visit user_path(@user)
+      expect(current_path).to eq(user_path(@user))
+
+      expect( page ).to have_content(@user.name)
+      expect( page ).to have_content(@post.title)
+      expect( page ).to have_content(@comment.body)
+    end
+  end
+
+  describe "sign in as admin" do
+    before { login_as authenticated_user(role: 'admin'), scope: :user }
     it "shows profile" do
       visit user_path(@user)
       expect(current_path).to eq(user_path(@user))
