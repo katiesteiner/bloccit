cfd799146d17dc6facc1ff91ac76f8031ffcac52
katiesteiner <katiewsteiner@gmail.com>
2016-02-22 22:46:24 -0500

    Checkpoint 58 Assignment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,7 +6,7 @@ class Comment < ActiveRecord::Base
   validates :body, presence: true
   validates :user_id, presence: true
 
-  default_scope { order('updated_at DESC') }
+  default_scope { order('updated_at ASC') }
 
   after_create :send_favorite_emails
 
